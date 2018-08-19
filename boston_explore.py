8538187295aeb45b41267f656c8be9886be0e127
Devanshu Agrawal <=>
2018-08-19 14:50:39 -0400

    Update boston_explore.py.

--- boston_explore.py
+++ boston_explore.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-import explore
+import explore_tools as et
 
-#explore.explained_variance_ratio(0, out="results")
-#explore.pca(0, n_components=8, out="results")
-#explore.corrs(0, out="results")
-#explore.plot_evr(0, max_k=10, out="results")
-#explore.plot_cross_sections(0, absolute=True, out="results")
-explore.plot_corrs(0, out="results")
+#et.explained_variance_ratio(0, out="results")
+#et.pca(0, n_components=8, out="results")
+#et.corrs(0, out="results")
+#et.plot_evr(0, max_k=10, out="results")
+#et.plot_cross_sections(0, absolute=True, out="results")
+et.plot_corrs(0, out="results")
 
 print("Done")
