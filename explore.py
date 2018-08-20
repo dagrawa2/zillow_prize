a5b168028381660fa59e6f95d47d08eb9447dcd5
DevanshuAgrawal <agrawald@goldmail.etsu.edu>
2018-08-20 09:05:51 -0400

    Fix corr color plot, add corr for absolute logerror.

--- explore.py
+++ explore.py
@@ -2,15 +2,15 @@ import numpy as np
 import pandas as pd
 import explore_tools as et
 
-year = 2017
+year = 2016
 out = "results/explore_"+str(year)
 
 #et.explained_variance_ratio(year, out=out)
-#et.pca(year, n_components=25, out=out)
-#et.corrs(year, out=out)
+et.pca(year, n_components=25, out=out)
+et.corrs(year, absolute=True, out=out)
 
 #et.plot_evr(year, max_k=10, out=out)
-et.plot_cross_sections(year, absolute=True, out=out)
+#et.plot_cross_sections(year, absolute=True, out=out)
 #et.plot_corrs(year, out=out)
 
 print("Done")
